41b7209e17fb6991f4f63312993953954b922c8b
Inge Wieser <ingewieser@gmail.com>
2017-08-02 19:05:06 -0700

    Merge two components from the main page (title and content)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-
-import ListBooksTitle from './components/ListBooksTitle'
 import ListBooksContent from './components/ListBooksContent'
 import SearchBooks from './components/SearchBooks'
 import * as BooksAPI from './BooksAPI'
@@ -9,7 +7,6 @@ import './App.css'
 
 
 class BooksApp extends React.Component {
-
   render() {
     return (
       <div className="app">
@@ -17,10 +14,7 @@ class BooksApp extends React.Component {
           <SearchBooks/>
         )}/>
         <Route exact path='/' render={() => (
-          <div className="list-books">
-            <ListBooksTitle/>
             <ListBooksContent/>
-          </div>
         )}/>
       </div>
     )
